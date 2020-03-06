c60a92227bbc1daa416d16e3260dfed98c9d4e86
Shivani Rana <mac@Macs-MacBook-Pro.local>
2020-03-06 12:41:39 +0545

    text added

--- src/include/ball.h
+++ src/include/ball.h
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 #ifndef VARIABLES_H
   #define VARIABLES_H
   #include"variables.h"
@@ -13,6 +15,7 @@
   #define OBSTACLE_H
 #endif
 
+
 void moveBall()
 {
   float ballAcc; // Ball acceleration
@@ -20,10 +23,23 @@ void moveBall()
       ballAcc=SPEED_4;
   }else if(ballz > roadLength*0.50){
     ballAcc=SPEED_3;
+   
   }else if(ballz > roadLength* 0.25){
     ballAcc=SPEED_2 ;
+  
   }else{
     ballAcc =SPEED_1;
+
+         // The character array to hole the string
+    char text[20] = "The text I want to print";
+        // The color, red for me
+      glColor3f(0, 0, 0);
+        // Position of the text to be printer
+        glRasterPos3f(0.0f, 2.0f, 1.0f);
+      for(int i = 0; text[i] != '\0'; i++){
+          glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+          }
+
   }
   ballz = ballz + ballAcc;
   //printf("Ballz: %f \t | \t Speed: %f\n",ballz,ballAcc);
@@ -37,7 +53,7 @@ void drawSphere()
     glPushMatrix();
     glColor3f(1.0f, 1.0f, 0.0f); 
     glTranslatef(ballx,0.5f,ballz);
-    glutSolidSphere(.5, 50,50);
+    glutSolidSphere(.4, 50,50);
     glPopMatrix();
 }
 
--- src/main.c
+++ src/main.c
@@ -19,15 +19,67 @@
 GLfloat x,y,z;
 int _h,_w;
 float camz;
+float xrot = -1.0, yrot = 0.50, zoom = 2.0;
 
 void init()
 {
-  glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
+  glClearColor(1.0f, 0.5f, 0.5f, 1.0f); 
   glClearDepth(1.0f);                   
   glEnable(GL_DEPTH_TEST);   
   glDepthFunc(GL_LEQUAL);    
   glShadeModel(GL_SMOOTH); 
 }
+
+
+
+void text()
+{
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+
+ glLoadIdentity();
+
+    glTranslatef(3, 0.5, ballz);
+
+    // Write the text before any transformation or rotation
+    // Or the text will change its position with the models
+
+  if(ballz> roadLength*0.75){
+    char text4[32];
+    sprintf(text4, "LEVEL 4", xrot, yrot);
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f( 0.3 , 3.0 , ballz);
+    for(int i = 0; text4[i] != '\0'; i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text4[i]);
+  }
+   if(ballz > roadLength*0.5){
+    char text3[32];
+    sprintf(text3, "LEVEL 3", xrot, yrot);
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f( 0.3 , 3.0 , ballz);
+    for(int i = 0; text3[i] != '\0'; i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text3[i]);
+  }
+
+   if(ballz > roadLength*0.25){
+    char text2[32];
+    sprintf(text2, "LEVEL 2", xrot, yrot);
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f( 0.3 , 3.0 , ballz);
+    for(int i = 0; text2[i] != '\0'; i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text2[i]);
+  }
+   else {
+    char text[32];
+    sprintf(text, "LEVEL 1", xrot, yrot);
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f( 3.0 , 3.0 , ballz);
+    for(int i = 0; text[i] != '\0'; i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+  }
+ 
+}
+
 
 void gradientBackground() // call this function and make bring this to the backgrounds
 {
@@ -59,6 +111,11 @@ void render()
   camz=ballz-5.5;
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
+
+    // Write the text before any transformation or rotation
+    // Or the text will change its position with the models
+    text();
+
   glLoadIdentity();
 
   glMatrixMode(GL_PROJECTION);
@@ -66,7 +123,7 @@ void render()
   gluPerspective(70.0f, ((GLdouble)_w/_h), 0.1f, 1000.0f);
 
   gluLookAt(0.0,3.0,camz,0.0,0.0,ballz,0.0,1.0,0.0);
-  drawRoad(500);
+  drawRoad(1000);
   drawObstacleBlock(10);
   drawSphere();
   drawFood(10);
