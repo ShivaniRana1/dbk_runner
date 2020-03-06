36a902e13411f94745cae23d49549b5a8b488288
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2020-03-06 10:18:38 +0545

    Food Placement done

--- src/include/food.h
+++ src/include/food.h
@@ -3,18 +3,42 @@
   #include"variables.h"
 #endif
 
+float foodMat[200][2];
+int isInitialized =0;
+void initFood(){
+    foodMat[0][0] = 0; // initializing the matrix so that it can be checked below if the value has been placed or not
+
+    float zLocation;
+    float xLocation;
+
+    for(int i =0; i < 200; i++){
+        zLocation = rand() % roadLength;
+        xLocation = (rand() % 3) -1;
+
+        foodMat[i][0] = xLocation;
+        foodMat[i][1] = zLocation;
+    }
+    isInitialized =1;
+
+}
 void drawFood(int number){
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glPushMatrix();
-    glTranslatef(0.0,1.0,10.0);
-    glRotatef(angleFood,0.0,1.0,0.0);
-    glutSolidTorus(0.1,0.5,4,4);
-    glPopMatrix();
+    if(!isInitialized){
+        initFood();
+    }
+    for(int i =0; i < 20; i++){
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glPushMatrix();
+        glTranslatef(foodMat[i][0] ,1.0,foodMat[i][1]);
+        glRotatef(angleFood,0.0,1.0,0.0);
+        glutSolidTorus(0.1,0.5,4,4);
+        glPopMatrix();
+       // printf("Z locations:%f\n",foodMat[i][1]);
+    }
+
 }
 
 void rotateFood(){
     angleFood += 5.0;
-    printf("%f\n",angleFood);
-
+    //printf("%f\n",angleFood);
 }
